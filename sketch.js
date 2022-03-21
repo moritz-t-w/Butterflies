d0a98c9abfb84d1088b99c34d47eda64618102b7
MoDerPo <90388353+moritz-t-w@users.noreply.github.com>
2022-03-21 10:52:54 +0100

    sorry didn't do any commits :/

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+let butterflies = [];
+
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(800, 400);
+}
+
+function mouseDragged() {
+    let r = random(20, 40);
+    let b = new Butterfly(mouseX, mouseY, r);
+    butterflies.push(b);
 }
 
 function draw() {
-  background(220);
+    background(0);
+    butterflies.forEach(butterfly => {
+        if ((butterfly.y > height || butterfly.y < 0) || (butterfly.x > width || butterfly.x < 0)) {
+            butterflies.splice(butterflies.indexOf(butterfly), 1)
+            console.log(butterflies.length + " butterflies")
+        } else {
+            butterfly.move();
+            butterfly.show();
+        }
+    });
+}
+
+class Butterfly {
+    constructor(tempX, tempY, tempRadius) {
+        this.x = tempX;
+        this.y = tempY;
+        this.radius = tempRadius;
+    }
+
+    move() {
+        this.x = this.x + random(-5, 5);
+        this.y = this.y - random(-3, 7);
+    }
+
+    show() {
+        stroke(random(1, 255), random(1, 255), random(1, 255)); // Zufalls-Rahmenfarben
+        strokeWeight(4);
+        fill(255, 160);
+        textSize(this.radius)
+        text("🦋", this.x, this.y);
+    }
 }
